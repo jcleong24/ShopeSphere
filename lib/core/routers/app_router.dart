ddee37d0d40aa5a65d2c3d13bd785574b785cf31
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-01 02:14:29 +0800

    Revert "Merge branch 'architecture/router_intergration' into feature/home_screen"
    This reverts commit 1851c4af1e9bafd1934b0d486ef87206db29701e, reversing
    changes made to dd01b311e4f1ba137574f66c2efa9b4b263da8cf.

--- lib/core/routers/app_router.dart
+++ lib/core/routers/app_router.dart
@@ -1,36 +1,14 @@
 import 'package:flutter_user_ecomm_app/core/routers/route_name.dart';
 import 'package:go_router/go_router.dart';
-import '../../screens/product_details_screen.dart';
-import '../../screens/cart_screen.dart';
-import '../../screens/checkout_screen.dart';
-import '../../screens/home_screen.dart';
-import '../../screens/payment_screen.dart';
-import '../../screens/product_details_screen.dart';
-// import '../../screens/splash_screen.dart';
+import '../../screens/splash_screen.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: RouteNames.productDetails,
+    initialLocation: RouteNames.splash,
     routes: [
       GoRoute(
-        path: RouteNames.home,
-        builder: (context, state) => const ProductListScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.productDetails,
-        builder: (context, state) => const ProductDetailsScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.cart,
-        builder: (context, state) => const CartScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.checkout,
-        builder: (context, state) => const CheckoutScreen(),
-      ),
-      GoRoute(
-        path: RouteNames.payment,
-        builder: (context, state) => const PaymentScreen(),
+        path: RouteNames.splash,
+        builder: (context, state) => const SplashScreen(),
       ),
     ],
   );

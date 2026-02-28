60a6ca030f386f58e11067b442d2d6782de00f6e
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-03-01 01:25:49 +0800

    added screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class ProductDetailsScreen extends StatelessWidget {
+  const ProductDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Details'),
+      ),
+      body: const Center(
+        child: Text('Product Details Screen'),
+      ),
+    );
+  }
+}
